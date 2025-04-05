0025293e97390a72f76b3c691a803375aeee160e
Devil_Alert <devidasdeo9@gmail.com>
2025-04-05 16:41:00 +0530

    React App Initialised. Also The chakra UI is installed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Box, Button } from '@chakra-ui/react'
+import { useState } from 'react'
+
+function App() {
+  return (
+    <Box minH={"100vh"} bg={'white'}>
+      
+    </Box>
+  )
+}
+
+export default App
